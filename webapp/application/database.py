e77f9c8598a6701964f9b2e1b36a2dfa96c7caf9
Mishra-raja <rajamishra0702@gmail.com>
2020-09-27 22:44:00 +0530

    added updated code for webapp

---
+++ webapp/application/database.py
@@ -0,0 +1,89 @@
+import sqlite3
+from sqlite3 import Error
+from datetime import datetime
+import time
+
+# CONSTANTS
+
+FILE = "messages.db"
+PLAYLIST_TABLE = "Messages"
+
+
+class DataBase:
+    """
+    used to connect, write to and read from a local sqlite3 database
+    """
+
+    def __init__(self):
+        """
+        try to connect to file and create cursor
+        """
+        self.conn = None
+        try:
+            self.conn = sqlite3.connect(FILE)
+        except Error as e:
+            print(e)
+
+        self.cursor = self.conn.cursor()
+        self._create_table()
+
+    def close(self):
+        """
+        close the db connection
+        :return: None
+        """
+        self.conn.close()
+
+    def _create_table(self):
+        """
+        create new database table if one doesn't exist
+        :return: None
+        """
+        query = f"""CREATE TABLE IF NOT EXISTS {PLAYLIST_TABLE}
+                    (name TEXT, content TEXT, time Date, id INTEGER PRIMARY KEY AUTOINCREMENT)"""
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    def get_all_messages(self, limit=100, name=None):
+        """
+        returns all messages
+        :param limit: int
+        :return: list[dict]
+        """
+        if not name:
+            query = f"SELECT * FROM {PLAYLIST_TABLE}"
+            self.cursor.execute(query)
+        else:
+            query = f"SELECT * FROM {PLAYLIST_TABLE} WHERE NAME = ?"
+            self.cursor.execute(query, (name,))
+
+        result = self.cursor.fetchall()
+
+        # return messages in sorted order by date
+        results = []
+        for r in sorted(result, key=lambda x: x[3], reverse=True)[:limit]:
+            name, content, date, _id = r
+            data = {"name": name, "message": content, "time": str(date)}
+            results.append(data)
+
+        return list(reversed(results))
+
+    def get_messages_by_name(self, name, limit=100):
+        """
+        Gets a list of messages by user name
+        :param name: str
+        :return: list
+        """
+        return self.get_all_messages(limit, name)
+
+    def save_message(self, name, msg):
+        """
+        saves the given message in the table
+        :param name: str
+        :param msg: str
+        :param time: datetime
+        :return: None
+        """
+        query = f"INSERT INTO {PLAYLIST_TABLE} VALUES (?, ?, ?, ?)"
+        self.cursor.execute(query, (name, msg, datetime.now(), None))
+        self.conn.commit()
---
+++ webapp/application/views.py
@@ -0,0 +1,135 @@
+from flask import Blueprint
+from flask import (
+    Flask,
+    render_template,
+    url_for,
+    redirect,
+    request,
+    session,
+    jsonify,
+    flash,
+    Blueprint,
+)
+from .database import DataBase
+
+view = Blueprint("views", __name__)
+
+
+# GLOBAL CONSTANTS
+NAME_KEY = "name"
+MSG_LIMIT = 20
+
+# VIEWS
+
+
+@view.route("/login", methods=["POST", "GET"])
+def login():
+    """
+    displays main login page and handles saving name in session
+    :exception POST
+    :return: None
+    """
+    if request.method == "POST":  # if user input a name
+        name = request.form["inputName"]
+        if len(name) >= 2:
+            session[NAME_KEY] = name
+            flash(f"You were successfully logged in as {name}.")
+            return redirect(url_for("views.home"))
+        else:
+            flash("1Name must be longer than 1 character.")
+
+    return render_template("login.html", **{"session": "session"})
+
+
+@view.route("/logout")
+def logout():
+    """
+    logs the user out by popping name from session
+    :return: None
+    """
+    session.pop(NAME_KEY, None)
+    flash("0You were logged out.")
+    return redirect(url_for("views.login"))
+
+
+@view.route("/")
+@view.route("/home")
+def home():
+    """
+    displays home page if logged in
+    :return: None
+    """
+    if NAME_KEY not in session:
+        return redirect(url_for("views.login"))
+
+    return render_template("index.html", **{"session": session})
+
+
+@view.route("/history")
+def history():
+    if NAME_KEY not in session:
+        flash("0Please login before viewing message history")
+        return redirect(url_for("views.login"))
+
+    json_messages = get_history(session[NAME_KEY])
+    print(json_messages)
+    return render_template("history.html", **{"history": json_messages})
+
+
+@view.route("/get_name")
+def get_name():
+    """
+    :return: a json object storing name of logged in user
+    """
+    data = {"name": ""}
+    if NAME_KEY in session:
+        data = {"name": session[NAME_KEY]}
+    return jsonify(data)
+
+
+@view.route("/get_messages")
+def get_messages():
+    """
+    :return: all messages stored in database
+    """
+    db = DataBase()
+    msgs = db.get_all_messages(MSG_LIMIT)
+    messages = remove_seconds_from_messages(msgs)
+
+    return jsonify(messages)
+
+
+@view.route("/get_history")
+def get_history(name):
+    """
+    :param name: str
+    :return: all messages by name of user
+    """
+    db = DataBase()
+    msgs = db.get_messages_by_name(name)
+    messages = remove_seconds_from_messages(msgs)
+
+    return messages
+
+
+# UTILITIES
+def remove_seconds_from_messages(msgs):
+    """
+    removes the seconds from all messages
+    :param msgs: list
+    :return: list
+    """
+    messages = []
+    for msg in msgs:
+        message = msg
+        message["time"] = remove_seconds(message["time"])
+        messages.append(message)
+
+    return messages
+
+
+def remove_seconds(msg):
+    """
+    :return: string with seconds trimmed off
+    """
+    return msg.split(".")[0][:-3]
